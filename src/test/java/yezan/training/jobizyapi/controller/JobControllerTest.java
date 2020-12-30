926bc48bd1ec2acf94547d6292af425a057ddbbd
YezanR <yezan.rafed@gmail.com>
2020-12-30 19:06:34 +0100

    Implement find matching jobs Controller

--- src/test/java/yezan/training/jobizyapi/controller/JobControllerTest.java
+++ src/test/java/yezan/training/jobizyapi/controller/JobControllerTest.java
@@ -1,13 +1,10 @@
 package yezan.training.jobizyapi.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import java.util.List;
@@ -22,6 +19,8 @@ import yezan.training.jobizyapi.factory.JobFactory;
 import yezan.training.jobizyapi.service.CandidateCrudService;
 import yezan.training.jobizyapi.service.JobMatcher;
 
+import javax.persistence.EntityNotFoundException;
+
 @WebMvcTest
 public class JobControllerTest {
 
@@ -35,15 +34,22 @@ public class JobControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void findAllMatchingJobs() throws Exception {
+    public void findAllMatchingJobs_shouldReturnJobs() throws Exception {
 
         List<Job> jobs = JobFactory.generateMany();
         when(jobMatcher.findAllMatchingJobs(any())).thenReturn(jobs);
 
         mockMvc.perform(get("/jobs/matchingCandidate/1"))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json("[{'title': 'IT Manager'}, {'title': 'Java developer'}, {'title': 'Angular tech lead'}]"));
+    }
+
+    @Test
+    public void findAllMatchingJobs_GivenInvalidCandidate_ShouldReturnNotFoundStatus() throws Exception {
+        when(candidateCrudService.findById(anyLong())).thenThrow(new EntityNotFoundException());
+
+        mockMvc.perform(get("/jobs/matchingCandidate/1"))
+                .andExpect(status().isNotFound());
     }
 }
